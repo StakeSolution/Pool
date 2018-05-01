998e7c19f69e5d8c4df232ab1ca9b0fa16dc9df4
DAQ-AART\AartStuurman <a.stuurman@solarteam.nl>
2018-05-01 14:32:37 +0200

    added events.

--- casper_fft.sol
+++ casper_fft.sol
@@ -8,11 +8,64 @@
 
 pragma solidity ^0.4.23;
 
-// TODO function descriptions
-// TODO events
+// TODO descriptions
 
 contract CasperFFT
 {
+    /** List of events the contract logs
+        Withdrawal address used always in _from and _to as it's unique TODO what does this mean?
+        and validator index is removed after some events TODO which events **/
+    // TODO description
+    event Deposit(
+        address indexed _from,
+        int128 indexed _validator_index,
+        address _validation_address,
+        int128 _start_dyn,
+        int128 _amount // wei
+    );
+    
+    // TODO description
+    event Vote(
+        address indexed _from,
+        int128 indexed _validator_index,
+        bytes32 indexed _target_hash,
+        int128 _start_dyn,
+        int128 _source_epoch
+    );
+    
+    // TODO description
+    event Logout(
+        address indexed _from,
+        int128 indexed _validator_index,
+        int128 _end_dyn
+    );
+    
+    // TODO description
+    event Withdraw(
+        address indexed _to,
+        int128 indexed _validator_index,
+        int128 _amount // wei
+    );
+    
+    // TODO description
+    event Slash(
+        address indexed _from,
+        address indexed _offender,
+        int128 indexed _offender_index,
+        int128 _bounty, // wei
+        int128 _destroyed // wei
+    );
+    
+    // TODO description
+    event Epoch(
+        int128 indexed _number,
+        bytes32 indexed _checkpoint_hash,
+        bool _is_justified,
+        bool _is_finalized
+    );
+
+    
+    // TODO description
     struct Validator
     {
         // Used to determine the amount of wei the validator holds. To get the actual
@@ -27,32 +80,33 @@ contract CasperFFT
         // The address to withdraw to
         address withdrawal_addr;
     }
-    
     mapping(int128 => Validator) public validators;
     
-    // Historical checkpoint hashes
+    // Historical checkpoint hashes TODO what does this mean?
     mapping(int128 => bytes32) public checkpoint_hashes;
     
     // Number of validators
     int128 public next_validator_index;
     
-    // Mapping of validator's withdrawal address to their index number
+    // Validator's withdrawal address => validator's index number
     mapping(int128 => address) public validator_indexes;
     
-    // Current dynasty, it measures the number of finalized checkpoints 
-    // in the chain from root to the parent of current block
+    /* Current dynasty. Measures the number of finalized checkpoints 
+       in the chain from root to the parent of current block */
     int128 public dynasty;
     
-    // Map of the change to total deposits for specific dynasty
+    // Dynasty => change to total deposits(wei/m) for the dynasty TODO what does this mean?
     mapping(int128 => fixed256x10) public dynasty_wei_delta; // wei/m
     
-    // Mapping of dynasty to start epoch of that dynasty
+    // Dynasty => start epoch for the dynasty
     mapping(int128 => int128) public dynasty_start_epoch;
 
-    // Mapping of epoch to what dynasty it is
+    // Epoch => Dynasty of epoch
     mapping(int128 => int128) public dynasty_in_epoch;
 
-    struct Vote
+    // TODO description
+    // Information about votes for an epoch.
+    struct Votes
     {
         // How many votes are there for this source epoch from the current dynasty
         mapping(int128 => fixed256x10) cur_dyn_votes; // wei/m
@@ -65,16 +119,17 @@ contract CasperFFT
         // Is a vote referencing the given epoch finalized?
         bool is_finalized;
     }
-    
-    // index: target epoch
-    mapping(int128 => Vote) public votes;
+    // Epoch => Votes
+    mapping(int128 => Votes) public votes;
 
-    // Is the current expected hash justified
+    // If the current expected hash is justified
     bool public main_hash_justified;
 
+    // TODO description
     // Value used to calculate the per-epoch fee that validators should be charged
     mapping(int128 => fixed256x10) public deposit_scale_factor; // unit: m
 
+    // TODO description
     fixed256x10 public last_nonvoter_rescale;
     fixed256x10 public last_voter_rescale;
 
@@ -99,6 +154,7 @@ contract CasperFFT
     // Logout delay in dynasties
     int128 public DYNASTY_LOGOUT_DELAY;
     
+    // TODO description    
     fixed256x10 BASE_INTEREST_FACTOR;
     fixed256x10 public BASE_PENALTY_FACTOR;
     int128 public MIN_DEPOSIT_SIZE; // TODO type wei_value ??
